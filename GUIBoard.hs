fc02462547909c7a46917cd160e1d8e4b72032e3
Artem Shvorin <art@shvorin.net>
2017-08-08 15:13:25 +0300

    a scratch of Haskell-implemented Hybrids chess board

---
+++ GUIBoard.hs
@@ -0,0 +1,62 @@
+module GUIBoard where
+
+import Graphics.UI.WX
+import Graphics.UI.WXCore.Events
+import Graphics.UI.WXCore.WxcTypes
+import Graphics.UI.WXCore.Draw
+
+main :: IO ()
+main = start gui
+
+gui :: IO ()
+gui = do
+  selected <- varCreate Nothing
+  
+  let img = image "bishopb.gif"
+  let bmap = bitmap "bishopb.gif"
+  let xsize = 96
+  let onPaint1 dc viewArea = do
+        drawRect dc (Rect (xsize-1) (xsize-1) (8*xsize+2) (8*xsize+2)) [ ]
+        let drawField col row = do
+              let colour = if (col + row) `mod` 2 == 0 then grey else white
+              drawRect dc (Rect (xsize*row) (xsize*col) xsize xsize) [ brush := brushSolid colour ]
+        sequence_ [drawField col row | col <- [1..8], row <- [1..8]]
+
+        sequence_ [drawText dc (show (9-col))
+                   (Point (xsize `div` 2) (xsize * col + xsize `div` 2)) [] | col <- [1..8]]
+        
+        sequence_ [drawText dc [" abcdefgh " !! row]
+                   (Point (xsize * row + xsize `div` 2) (xsize * 9 + xsize `div` 2)) [] | row <- [1..8]]
+
+  let onPaint dc viewArea = do
+        onPaint1 dc viewArea
+        p <- varGet selected
+        case p of
+          Just pos -> do
+            drawBitmap dc bmap pos True [] 
+            circle dc pos 20 [brush := brushSolid red]
+          Nothing -> return ()
+
+  let onClick point = varSet selected $ Just point
+  let onUnclick point = varSet selected Nothing
+  let onDrag point = do
+        putStrLn $ "on mouse: " ++ show point
+        p <- varGet selected
+        case p of
+          Nothing -> return ()
+          Just _ -> varSet selected (Just point)
+  
+  f <- frame [ resizeable := False ]
+  p <- panel f [on keyboard      := \k -> (putStrLn $ "key: " ++ show k)
+               , on paint := onPaint
+               , on click := onClick
+               , on unclick := onUnclick
+               , on drag := onDrag
+--               , bgcolor := white
+               ]
+  t <- timer f [ interval   := 50
+               , on command := repaint p
+               ]
+  set f [layout := boxed "foo" $ container p $ space (10*xsize) (10*xsize)
+        ]
+  return ()
---
+++ Pieces.hs
@@ -0,0 +1,179 @@
+module Pieces where
+
+import Data.Array.Unboxed
+import GHC.Read
+import Text.ParserCombinators.ReadPrec (readP_to_Prec, readPrec_to_P)
+import qualified Data.Char
+import Text.ParserCombinators.ReadP
+
+data Field = Field (Int, Int) deriving (Eq, Ord, Ix)
+
+mkReadPrec r = readP_to_Prec (\_ -> r)
+mkReadP r = readPrec_to_P r 0
+
+instance Show Field where
+  show (Field (x,y)) = [files !! x] ++ show y where
+    files = ".abcdefgh"
+
+ranks_range = ('1','8')
+files_range = ('a','h')
+
+instance Read Field where
+  readPrec = mkReadPrec $ do
+    f <- readFileOrRank files_range
+    r <- readFileOrRank ranks_range
+    return $ Field (f,r)
+      where
+        readFileOrRank pair = do
+          c <- get
+          if inRange pair c then
+            return (index pair c + 1)
+            else pfail
+
+instance Show Piece where
+  show K = "K"
+  show P = "p"
+  show (Hybrid p1 p2) = show p1 ++ show p2
+  show (Prime p) = show p
+
+instance Read Piece where
+  readPrec = mkReadPrec $ getHybridable <++ getOther <++ return P where
+    getPrime = mkReadP readPrec -- do
+      -- c <- get
+      -- case c of
+      --   'G' -> return $ Prime G
+      --   'R' -> return $ Prime R
+      --   'B' -> return $ Prime B
+      --   'N' -> return $ Prime N
+      --   _ -> pfail
+    getHybridable = do
+      p@(Prime p1) <- getPrime
+      ((\(Prime p2) -> Hybrid p1 p2) `fmap` getPrime) <++ return p
+    getOther = do
+      c <- get
+      case c of
+        'K' -> return K
+        'p' -> return P
+        _ -> pfail
+
+data Piece = K | P | Hybrid Prime Prime | Prime Prime deriving Eq
+data Prime = R | B | N | G deriving (Eq, Show)
+data Color = Black | White deriving (Eq, Show)
+data Item = Piece Color Piece | Empty deriving Show
+
+data Board = Board (Array Field Item) deriving Show
+
+data Vector = Vector (Int, Int)
+
+add :: Field -> Vector -> Field
+add (Field (x, y)) (Vector (u, v)) = Field (x+u, y+v)
+
+rot90 (x,y) = (-y, x)
+rep4 v = take 4 $ rep v where
+  rep v = v:rep (rot90 v)
+
+attacks :: Piece -> [Vector]
+attacks = map Vector . attacks' where
+  attacks' piece = case piece of
+                     Prime R -> rep4 (0,1)
+                     Prime B -> rep4 (1,1)
+                     Prime N -> rep4 (1,2) ++ rep4 (2,1)
+                     Hybrid p1 p2 -> attacks' (Prime p1) ++ attacks' (Prime p2)
+                     P -> [(1,1), (-1,1)]
+                     _ | piece `elem` [Prime G, K] -> rep4 (0,1) ++ rep4 (1,1)
+
+land :: (Piece, Color) -> Item -> Maybe Item
+land (actor, color) target =
+  case target of
+    Empty -> Just $ Piece color actor
+    Piece targetColor targetPiece ->
+      if color == targetColor then
+        case (actor, targetPiece) of
+          -- join
+          (Prime p1, Prime p2) -> Just $ Piece color (Hybrid p1 p2)
+          _ -> Nothing
+      else
+        -- capture
+        Just $ Piece color actor
+
+emptyBoard = Board $ listArray (Field (1,1), Field (8,8)) (repeat Empty)
+
+--initialBoard = emptyBoard // []
+
+iterMoves :: Piece -> Board -> Field -> [Field]
+iterMoves piece (Board board) src =
+  do
+    v <- attacks piece
+    let foo x =
+          let
+            y = add x v
+          in
+            if inRange (bounds board) y then
+              case board ! y of
+                Empty | isRanger piece -> y : foo y
+                _ -> [y]
+            else
+              []
+            
+    x <- foo src
+    return x
+
+isRanger piece = case piece of
+                  Prime p -> isRanger p
+                  Hybrid p1 p2 -> isRanger p1 || isRanger p2
+                  _ -> False
+  where
+    isRanger R = True
+    isRanger B = True
+    isRanger _ = False
+
+readItem = between (char '(') (char ')') readHybrid <++ readSingle where
+  readSingle = do
+    c <- get
+    piece <- do
+      case Data.Char.toUpper c of
+        'N' -> return $ Prime N
+        'G' -> return $ Prime G
+        'R' -> return $ Prime R
+        'B' -> return $ Prime B
+        'K' -> return K
+        'P' -> return P
+        _   -> pfail
+    return $ Piece (if Data.Char.isLower c then White else Black) piece
+
+  readHybrid = do
+    (c1, p1) <- readPrime
+    (c2, p2) <- readPrime
+    if c1 == c2
+      then return $ Piece c1 $ Hybrid p1 p2
+      else pfail
+
+  readPrime = do
+    c <- get
+    prime <- do
+      case Data.Char.toUpper c of
+        'N' -> return N
+        'G' -> return G
+        'R' -> return R
+        'B' -> return B
+        _   -> pfail
+    return (if Data.Char.isLower c then White else Black, prime)
+    
+readDigit (f, r, acc) = do
+  c <- get
+  if inRange ranks_range c then
+    let r' = index ranks_range c + 1 in
+      readFEN (f, r + r', acc)
+    else pfail
+
+readFEN state@(f, r, acc) = do
+  case (f, r) of
+    (7, 8) -> return acc
+    (_, 8) -> char '/' >> readFEN(f+1, 0, acc)
+    _ -> (readItem >>= (\item -> readFEN(f, r+1, (Field (f+1, r+1), item):acc))) <++ readDigit state
+
+-- reads FEN
+instance Read Board where
+  readPrec = mkReadPrec $ do
+    ls <- readFEN (0, 0, [])
+    return $ Board (listArray (Field (1,1), Field (8,8)) (repeat Empty) // ls)
